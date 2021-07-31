8ba6463faf2475cab7d4710098ab9a0756bf58e3
Evan Mora <evanrmora@gmail.com>
2021-07-31 16:36:47 -0600

    udated routes, server.js

---
+++ routes/apiroutes.js
@@ -0,0 +1,52 @@
+const db = require('../models');
+
+module.exports = function(app) {
+    // Pull up info for workouts
+    app.get('/api/workouts', (req, res) => {
+        db.Workout.findAll().then(dbWorkout => {
+            res.send(dbWorkout);
+        })
+        .catch(err => {
+            res.send(err)
+        });
+    });
+
+    // Get range
+    app.get('/api/workouts/range',({}, res) => {
+        db.Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.send(err)
+        });
+    });
+
+    // submits a new workout
+    app.post('/api/workouts', (req, res) => {
+        db.Workout.create(req.body).then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.send(err)
+        });
+    });
+
+
+    // Send workout info
+    app.put('/api/workouts/:id', (req, res) => {
+        db.Workout.findByIdAndUpdate({_id: req.params.id},{ exercises: req.body}).then(dbWorkout => {
+            res.json(dbWorkout);
+        }
+        )
+        .catch(err => {
+            res.send(err)
+        }
+        );
+    });
+};
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+// Create an express app
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Connect mongoose db
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout',{
+    newUrlParser: true,
+    useFindAndModify: false,
+});
+
+// Set up routes
+require('./routes/htmlroutes')(app);
+require('./routes/apiroutes')(app);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+}
+);
